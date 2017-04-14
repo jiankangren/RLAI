b5a5198992cf4d71c3819c28576fed95c0efa3ec
zzzz94 <zz028113@foxmail.com>
2017-04-14 21:16:02 +0800

    update

--- chapter05/BlackJack.py
+++ chapter05/BlackJack.py
@@ -13,11 +13,12 @@ DRAW = 2
 ACTIONS = [ACTION_HIT, ACTION_STICK]
 
 policy_player = np.zeros((22, 11, 2))
+
 policy_dealer = np.zeros(22)
 for i in range(12, 22):
     for j in range(1, 11):
         for k in range(2):
-            if i < 21:
+            if i < 20:
                 policy_player[i][j][k] = ACTION_HIT
             else:
                 policy_player[i][j][k] = ACTION_STICK
@@ -41,24 +42,36 @@ def get_card():
     card = np.random.randint(1, 14)
     return min(card, 10)
 
-def play(state = None, action = None):
+def play(state = None, action = None, figure5_4 = False):
     state_action_pairs = []
     if state is None and action is None:
-        player_usable_ace = USABLE_ACE
-        player_sum = 0
-        player_card1 = get_card()
-        while player_sum < 12:
-            player_card2 = get_card()
+        if figure5_4:
+            rho = 1.
+            player_sum = 13
             player_usable_ace = USABLE_ACE
-            if player_card1 == 1 and player_card2 == 1:
-                player_sum = 12
-            elif player_card1 == 1 and player_card2 != 1:
-                player_sum = 11 + player_card2
-            elif player_card1 != 1 and player_card2 == 1:
-                player_sum = player_card1 + 11
-            else:
-                player_sum = player_card1 + player_card2
-                player_usable_ace = NO_USABLE_ACE
+            dealer_card1 = 2
+            while np.random.binomial(1, 0.5):
+                state_action_pairs.append([(player_sum, dealer_card1, player_usable_ace), ACTION_HIT])
+                player_sum += get_card()
+                if player_sum > 21:
+                    return -1, state_action_pairs
+            state_action_pairs.append([(player_sum, dealer_card1, player_usable_ace), ACTION_STICK])
+        else:
+            player_usable_ace = USABLE_ACE
+            player_sum = 0
+            player_card1 = get_card()
+            while player_sum < 12:
+                player_card2 = get_card()
+                player_usable_ace = USABLE_ACE
+                if player_card1 == 1 and player_card2 == 1:
+                    player_sum = 12
+                elif player_card1 == 1 and player_card2 != 1:
+                    player_sum = 11 + player_card2
+                elif player_card1 != 1 and player_card2 == 1:
+                    player_sum = player_card1 + 11
+                else:
+                    player_sum = player_card1 + player_card2
+                    player_usable_ace = NO_USABLE_ACE
             dealer_card1 = get_card()
     else:
         player_sum, dealer_card1, player_usable_ace = state
@@ -98,24 +111,14 @@ def play(state = None, action = None):
         else:
             dealer_sum += card
         if dealer_sum > 21:
-            if state is not None and action is not None:
                 return 1, state_action_pairs
-            else:
-                return PLAYER_WIN, player_init_ace, player_init_sum, dealer_card1
-    if state is not None and action is not None:
-        if player_sum == dealer_sum:
-            return 0, state_action_pairs
-        elif player_sum > dealer_sum:
-            return 1, state_action_pairs
-        else:
-            return -1, state_action_pairs
+
+    if player_sum == dealer_sum:
+        return 0, state_action_pairs
+    elif player_sum > dealer_sum:
+        return 1, state_action_pairs
     else:
-        if player_sum == dealer_sum:
-            return DRAW, player_init_ace, player_init_sum, dealer_card1
-        elif player_sum > dealer_sum:
-            return PLAYER_WIN, player_init_ace, player_init_sum, dealer_card1
-        else:
-            return DEALER_WIN, player_init_ace, player_init_sum, dealer_card1
+        return -1, state_action_pairs
 
 def mc_eval(ES = False):
     global state_values_not_usable_ace, state_values_usable_ace
@@ -201,8 +204,86 @@ def display5_3():
     display(value_usable_ace, ['player sum', 'dealer show', 'state-value value'], 'usable ace')
     display(value_no_usable_ace, ['player sum', 'dealer show', 'state-value value'], 'no usable ace')
 
-mc_ctrl(1000000)
-display5_3()
+def off_policy(n_episodes):
+    ord_value = [0]
+    weighted_value = [0]
+    sum_of_rhos = [0]
+    sum_of_rewards = [0]
+    for i in range(n_episodes):
+        reward, pairs = play(figure5_4=True)
+        above = below = 1.
+        for pair in pairs:
+            player_sum, dealer_show, usable_ace = pair[0]
+            action = pair[1]
+            if action == policy_player[player_sum, dealer_show, usable_ace]:
+                below *= 0.5
+            else:
+                above = 0
+                break
+        rho = above / below
+        sum_of_rhos.append(sum_of_rhos[-1] + rho)
+        sum_of_rewards.append(ord_value[-1] + rho * reward)
+
+    del sum_of_rewards[0]
+    del sum_of_rhos[0]
+
+    sum_of_rhos = np.asarray(sum_of_rhos)
+    sum_of_rewards = np.asarray(sum_of_rewards)
+
+    ord_value = sum_of_rewards / np.arange(1, n_episodes + 1)
+    weighted_value = np.where(sum_of_rhos != 0, sum_of_rewards / sum_of_rhos, 0)
+    return ord_value, weighted_value
+
+def display5_4(n_episodes):
+    target = -0.27726
+    run = 100
+    ord_value =  np.zeros(n_episodes)
+    weighted_value = np.zeros(n_episodes)
+    for i in range(run):
+        temp1, temp2 = off_policy(n_episodes)
+        ord_value += temp1
+        weighted_value += temp2
+    ord_value /= run
+    weighted_value /= run
+    ord_sampling = np.power(ord_value - target, 2)
+    weighted_sampling = np.power(weighted_value - target, 2)
+    axis_x = np.log10(np.arange(1, n_episodes + 1))
+    plt.figure()
+    plt.plot(axis_x, ord_sampling, label = 'ordinary sampling')
+    plt.plot(axis_x, weighted_sampling, label = 'weighted sampling')
+    plt.xlabel('episodes(10^x)')
+    plt.ylabel('mean square')
+    plt.legend()
+
+def display5_4_2(n_episodes):
+    target = -0.27726
+    run = 100
+    ord_sampling = np.zeros(n_episodes)
+    weighted_sampling = np.zeros(n_episodes)
+    for i in range(run):
+        temp1, temp2 = off_policy(n_episodes)
+        ord_sampling += np.power(temp1 - target, 2)
+        weighted_sampling += np.power(temp2 - target, 2)
+    ord_sampling /= run
+    weighted_sampling /= run
+
+    axis_x = np.log10(np.arange(1, n_episodes + 1))
+    plt.figure()
+    plt.plot(axis_x, ord_sampling, label='ordinary sampling')
+    plt.plot(axis_x, weighted_sampling, label='weighted sampling')
+    plt.xlabel('episodes(10^x)')
+    plt.ylabel('mean square')
+    plt.legend()
+
+
+
+
+
+
+#mc_ctrl(500000)
+#display5_3()
+display5_4(10000)
+display5_4_2(10000)
 plt.show()
 
 
